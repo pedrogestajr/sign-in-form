df4242399ce5b42c28130f25e20c663d1add2fe7
Pedro Gesta <pedrogestajr@gmail.com>
2022-06-28 21:44:19 -0300

    Add custom validation script
    - Implement password input validation
    
    - Implement form submission handler

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,7 @@
 const passwordInput = document.getElementById("password");
 const togglePasswordButton = document.getElementById("toggle-password");
+const form = document.querySelector("form");
+const signinButton = document.querySelector("button#sign-in");
 
 togglePasswordButton.addEventListener("click", togglePassword);
 
@@ -15,5 +17,40 @@ function togglePassword() {
       "aria-label",
       "Show password as plain text. Warning: This will display your password on the screen."
     );
+  }
+}
+
+passwordInput.addEventListener("input", resetCustomValidity);
+
+function resetCustomValidity() {
+  passwordInput.setCustomValidity("");
+}
+
+// Basic password validation
+function validatePassword() {
+  let message = "Your password should contain: ";
+  if (!/.{8,}/.test(passwordInput.value)) {
+    message += "eight or more characters; ";
+  }
+  if (!/.*[A-Z].*/.test(passwordInput.value)) {
+    message += "at least one uppercase letter; ";
+  }
+  if (!/.*[a-z].*/.test(passwordInput.value)) {
+    message += "at least one lowercase letter;";
+  }
+  passwordInput.setCustomValidity(message);
+}
+
+form.addEventListener("submit", handleFormSubmission);
+
+function handleFormSubmission(event) {
+  event.preventDefault();
+  validatePassword();
+  form.reportValidity();
+  if (form.checkValidity() === false) {
+  } else {
+    // Simulating user login when inputs are valid
+    alert("Logging in!");
+    signinButton.disabled = "true";
   }
 }
